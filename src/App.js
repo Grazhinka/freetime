5a02395eb836f24e6e98ae4787c8a68745b0b7b1
Grazhinka <gr-km@mail.ru>
2022-10-11 23:55:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import { useEffect,useState } from 'react';
 import './App.css';
+import relax from './relax.png'
+import education from './education.png'
+import cook from './cook.png'
+import soc from './soc.png'
+import busy from './busy.png'
+import char from './char.png'
+import music from './music.png'
+import hea from './hea.png'
+import diy from './diy.png'
+import els from './else.png'
 
-function App() {
+function App() {  
+    const [activityApi,setActivityApi]=useState('')
+    const [img,setImg]=useState('')
+    const api=async()=>{
+    const response=await fetch('http://www.boredapi.com/api/activity/')
+    const data= await response.json()
+    setActivityApi(data.activity)
+    console.log(data.type)
+  if(data.type==='relaxation'){
+    setImg(relax)
+  }
+  else if(data.type==='education'){
+    setImg(education)
+  }
+  else if(data.type==='cooking'){
+    setImg(cook)
+  }
+  else if(data.type==='social'){
+    setImg(soc)
+  }
+  else if(data.type==='diy'){
+    setImg(diy)
+  }
+  else if(data.type==='music'){
+    setImg(music)
+  }
+  else if(data.type==='recreational'){
+    setImg(hea)
+  }
+  else if(data.type==='busywork'){
+    setImg(busy)
+  }
+  else if(data.type==='charity'){
+    setImg(char)
+  }
+  else{
+    setImg(els)
+  }
+}
+  useEffect(()=>{
+  api()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <h1>Ideas for free time</h1>
+      <h2>{activityApi}</h2>
+      <div className='height'><img alt='img' src={img}/></div>
+      <button onClick={api}>a new idea</button>
     </div>
   );
 }
