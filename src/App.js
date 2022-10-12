58da12386611e0d830635f796b3b40d197be7599
Grazhinka <gr-km@mail.ru>
2022-10-12 12:14:14 +0300

    api

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     const [activityApi,setActivityApi]=useState('')
     const [img,setImg]=useState('')
     const api=async()=>{
-    const response=await fetch('http://www.boredapi.com/api/activity/')
+    const response=await fetch('https://www.boredapi.com/api/activity/')
     const data= await response.json()
     setActivityApi(data.activity)
     console.log(data.type)
